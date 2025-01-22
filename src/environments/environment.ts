33e730ca519cf8367dbf068d032e56295775f8e1
Rodrigo Sntg <digo_santiago@hotmail.com>
2025-01-22 01:01:15 -0300

    Adding tests

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: false,
+    cognito: {
+        tokenUrl: 'https://cutme.auth.us-east-1.amazoncognito.com/oauth2/token',
+        clientId: '3f5nviob425f0tjhhljmsm7n8p',
+        userPoolId: 'us-east-1_POLxfwFzO',
+        userPoolWebClientId: '3f5nviob425f0tjhhljmsm7n8p',
+        redirectUri: 'http://localhost:4200/callback',
+        cognitoDomain: 'https://cutme.auth.us-east-1.amazoncognito.com',
+        scope: 'openid profile email',
+        responseType: 'code'
+      },
+      apiBaseUrl: "http://localhost:8080"
+  };
+  
