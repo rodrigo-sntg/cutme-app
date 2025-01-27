cb56dcc39226bcb52b680251a950eb30b7e4c0fc
Robson Souza <robsontrips@gmail.com>
2025-01-26 22:11:40 -0300

    feat: atualizado ci/cd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,14 @@
 export const environment = {
-    production: false,
-    cognito: {
-        tokenUrl: 'https://cutme.auth.us-east-1.amazoncognito.com/oauth2/token',
-        clientId: '3f5nviob425f0tjhhljmsm7n8p',
-        userPoolId: 'us-east-1_POLxfwFzO',
-        userPoolWebClientId: '3f5nviob425f0tjhhljmsm7n8p',
-        redirectUri: 'http://localhost:4200/callback',
-        cognitoDomain: 'https://cutme.auth.us-east-1.amazoncognito.com',
-        scope: 'openid profile email',
-        responseType: 'code'
-      },
-      apiBaseUrl: "http://localhost:8080"
-  };
-  
+  production: false,
+  cognito: {
+      tokenUrl: 'https://cutme.auth.us-east-1.amazoncognito.com/oauth2/token',
+      clientId: '3f5nviob425f0tjhhljmsm7n8p',
+      userPoolId: 'us-east-1_POLxfwFzO',
+      userPoolWebClientId: '3f5nviob425f0tjhhljmsm7n8p',
+      redirectUri: 'http://localhost:4200/callback',
+      cognitoDomain: 'https://cutme.auth.us-east-1.amazoncognito.com',
+      scope: 'openid profile email',
+      responseType: 'code'
+    },
+    apiBaseUrl: "http://api-cutme-load-balancer-1601953081.us-east-1.elb.amazonaws.com"
+};
