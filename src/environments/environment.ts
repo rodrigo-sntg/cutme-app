17a36e0c7dabfc3007acb80dcf11648fca5ce2fc
Robson Souza <robsontrips@gmail.com>
2025-02-04 21:22:42 -0300

    feat: setup cdn

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@ export const environment = {
       clientId: '3f5nviob425f0tjhhljmsm7n8p',
       userPoolId: 'us-east-1_POLxfwFzO',
       userPoolWebClientId: '3f5nviob425f0tjhhljmsm7n8p',
-      redirectUri: 'http://localhost:4200/callback',
+      redirectUri: 'http://cutme.app.s3-website-us-east-1.amazonaws.com/callback',
       cognitoDomain: 'https://cutme.auth.us-east-1.amazoncognito.com',
       scope: 'openid profile email',
       responseType: 'code'
     },
-    apiBaseUrl: "http://api-cutme-load-balancer-1601953081.us-east-1.elb.amazonaws.com"
+    apiBaseUrl: "http://api-cutme-load-balancer-900796614.us-east-1.elb.amazonaws.com"
 };
